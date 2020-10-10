14fa4c04026ebb7565caea5b316751e5a93b59ee
Anwesh43 <anweshthecool0@gmail.com>
2020-10-11 04:03:36 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedprogressivearcview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedprogressivearcview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedprogressivearcview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.progressivearcview.ProgressArcView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ProgressArcView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
