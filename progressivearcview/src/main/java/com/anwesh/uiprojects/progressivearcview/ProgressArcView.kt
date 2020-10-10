c7e6664c8e2bfaff704b22980fcbd3ef7a3dcf79
Anwesh43 <anweshthecool0@gmail.com>
2020-10-11 03:20:04 +0530

    adding constants and libraries

--- progressivearcview/src/main/java/com/anwesh/uiprojects/progressivearcview/ProgressArcView.kt
+++ progressivearcview/src/main/java/com/anwesh/uiprojects/progressivearcview/ProgressArcView.kt
@@ -4,3 +4,26 @@ package com.anwesh.uiprojects.progressivearcview
  * Created by anweshmishra on 11/10/20.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+        "#F44336",
+        "#673AB7",
+        "#4CAF50",
+        "#2196F3",
+        "#4CAF50"
+).map({Color.parseColor(it)}).toTypedArray()
+val parts : Int = 2
+val sizeFactor : Float = 6.9f
+val strokeFactor : Float = 90f
+val scGap : Float = 0.02f / parts
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+
