47a23746ef24a40ff07375af8f36bd3c5a35e229
Anwesh43 <anweshthecool0@gmail.com>
2020-10-11 03:34:13 +0530

    adding Canvas extension methods

--- progressivearcview/src/main/java/com/anwesh/uiprojects/progressivearcview/ProgressArcView.kt
+++ progressivearcview/src/main/java/com/anwesh/uiprojects/progressivearcview/ProgressArcView.kt
@@ -31,3 +31,27 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawProgressArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    val fontSize : Float = size / 5
+    val tx : String = "${Math.floor(100.0 * scale).toInt()}"
+    val tw : Float = paint.measureText(tx) / 2
+    paint.textSize = fontSize
+    paint.style = Paint.Style.STROKE
+    save()
+    translate(w / 2, h / 2)
+    drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), -90f, 360f * sf1, false, paint)
+    drawText(tx, -tw / 2, -fontSize / 4, paint)
+    restore()
+}
+
+fun Canvas.drawPANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawProgressArc(scale, w, h, paint)
+}
